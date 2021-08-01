4b00165d67c6913a7cfea490318ebdb0fa32cdce
Nikita Kryukov <cravtos92@gmail.com>
2021-08-01 15:32:24 +0700

    Add proxy support
    Two new flags added:
    	-proxy: Path to text file containing one proxy per line.
    Program starts one bot per one proxy. If file with proxies is not
    specified, proxy from $HTTP_PROXY used (default behaviour of
    http.DefaultClient). Supported proxies: http, https, socks5.
    	-timeout: Time given to the bot for one request.

--- bots/shop-fake-review/shop-fake-review.go
+++ bots/shop-fake-review/shop-fake-review.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
@@ -9,8 +10,10 @@ import (
 	"math/rand"
 	"net/http"
 	"net/url"
+	"os"
 	"regexp"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/pkg/errors"
@@ -31,10 +34,13 @@ var (
 
 func main() {
 	productLink := flag.String("product", "", "Link to the product for which to leave fake reviews for.")
-	reviewAmount := flag.Uint("n", 10, "Amount of reviews to leave.")
+	reviewAmount := flag.Uint("n", 1, "Amount of reviews to leave from single proxy.")
 	maxAttempts := flag.Uint("attempts", 3, "Amount of attempts allowed to send single review. Zero means no limit.")
 	rating := flag.Int("rating", 5, "Rating for the reviews (1-5).")
-
+	proxyPath := flag.String("proxy", "", "Path to text file containing one proxy per line. "+
+		"Supported schemes: http, https, socks5. Line example: socks5://0.0.0.0:1337."+
+		"If no path specified or no proxies in file present, proxy from $HTTP_PROXY is used.")
+	reqTimeout := flag.Duration("timeout", 10*time.Second, "The time given to the bot for one request.")
 	flag.Parse()
 
 	if *productLink == "" || *reviewAmount <= 0 {
@@ -53,60 +59,140 @@ func main() {
 		log.Fatal(err)
 	}
 
-	postID, err := getPostID(*productLink)
+	proxies, err := scanProxies(*proxyPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Get postID using first proxy in the given list, or without proxy if no proxies are given
+	var postID string
+	if proxies == nil || len(proxies) == 0 {
+		client := &http.Client{Timeout: *reqTimeout}
+		postID, err = getPostID(client, *productLink)
+	} else {
+		client := &http.Client{
+			Transport: &http.Transport{
+				Proxy: http.ProxyURL(proxies[0]),
+			},
+			Timeout: *reqTimeout,
+		}
+		postID, err = getPostID(client, *productLink)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wg := &sync.WaitGroup{}
+
+	// Case where no proxies are given
+	// WaitGroup here is redundant, but present to reuse startBot function with no proxies
+	if proxies == nil || len(proxies) == 0 {
+		client := &http.Client{Timeout: *reqTimeout}
+		wg.Add(1)
+		startBot(wg, client, prodURL, postID, *rating, *maxAttempts, *reviewAmount, 0)
+		return
+	}
+
+	for botID, proxy := range proxies {
+		client := &http.Client{
+			Transport: &http.Transport{
+				Proxy: http.ProxyURL(proxy),
+			},
+			Timeout: *reqTimeout,
+		}
+		wg.Add(1)
+		go startBot(wg, client, prodURL, postID, *rating, *maxAttempts, *reviewAmount, botID)
+	}
+
+	wg.Wait()
+}
+
+func scanProxies(proxyPath string) ([]*url.URL, error) {
+	var proxies []*url.URL
+
+	if proxyPath == "" {
+		return nil, nil
+	}
+
+	proxyFile, err := os.Open(proxyPath)
+	if err != nil {
+		return nil, err
+	}
+	defer proxyFile.Close()
+
+	scanner := bufio.NewScanner(proxyFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		proxy, err := url.Parse(line)
+		if err != nil {
+			return nil, err
+		}
+		proxies = append(proxies, proxy)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return proxies, nil
+}
+
+func startBot(wg *sync.WaitGroup, client *http.Client, prodURL *url.URL, postID string, rating int, maxAttempts, reviewAmount uint, botID int) {
 	timeoutDefault := 15 * time.Second
 	var reviewsDone uint
 	var attempts uint
 	for { // for reviewsDone < *reviewAmount
-		log.Println("Sending review")
-		err := leaveReview(prodURL.Scheme, prodURL.Host, postID, *rating)
+		log.Printf("[BOT-%d] Sending reviev\n", botID)
+		err := postReview(client, prodURL.Scheme, prodURL.Host, postID, rating)
 		switch err {
 		case ErrDuplicate:
-			log.Println("Tried to send duplicate review")
+			log.Printf("[BOT-%d] Tried to send duplicate review\n", botID)
 
 		case ErrMaintenance:
-			log.Println("Site under maintenance")
+			log.Printf("[BOT-%d] Site under maintenance\n", botID)
 			attempts++
-			if *maxAttempts != 0 && attempts >= *maxAttempts {
-				log.Println("Attempts exceeded")
+			if maxAttempts != 0 && attempts >= maxAttempts {
+				log.Printf("[BOT-%d] Attempts exceeded\n", botID)
+				wg.Done()
 				return
 			}
-			log.Printf("Sleeping for %v\n", timeoutDefault)
+			log.Printf("[BOT-%d] Sleeping for %v\n", botID, timeoutDefault)
 			time.Sleep(timeoutDefault)
 
 		case ErrTooQuickly:
-			log.Println("Got timed out")
+			log.Printf("[BOT-%d] Got timed out\n", botID)
 			attempts++
-			if *maxAttempts != 0 && attempts >= *maxAttempts {
-				log.Println("Attempts exceeded")
+			if maxAttempts != 0 && attempts >= maxAttempts {
+				log.Printf("[BOT-%d] Attempts exceeded\n", botID)
+				wg.Done()
 				return
 			}
 			timeout := timeoutDefault + time.Duration(rand.Intn(5))*time.Second
-			log.Printf("Sleeping for %v\n", timeout)
+			log.Printf("[BOT-%d] Sleeping for %v\n", botID, timeout)
 			time.Sleep(timeout)
 
 		case nil:
 			reviewsDone++
 			attempts = 0
-			log.Printf("Sent review")
-			if reviewsDone >= *reviewAmount {
+			log.Printf("[BOT-%d] Sent review\n", botID)
+			if reviewsDone >= reviewAmount {
+				wg.Done()
 				return
 			}
-			log.Printf("Sleeping for %v\n", timeoutDefault)
+			log.Printf("[BOT-%d] Sleeping for %v\n", botID, timeoutDefault)
 			time.Sleep(timeoutDefault)
 
 		default:
-			log.Fatal(err)
+			log.Printf("[BOT-%d] Got error: %v", botID, err)
+			wg.Done()
+			return
 		}
 	}
 }
 
-func getPostID(productLink string) (string, error) {
-	resp, err := http.DefaultClient.Get(productLink)
+func getPostID(client *http.Client, productLink string) (string, error) {
+	resp, err := client.Get(productLink)
 	if err != nil {
 		return "", err
 	}
@@ -129,7 +215,7 @@ func getPostID(productLink string) (string, error) {
 	return prodID, nil
 }
 
-func leaveReview(scheme, host, postID string, rating int) error {
+func postReview(client *http.Client, scheme, host, postID string, rating int) error {
 	to := fmt.Sprintf("%s://%s/wp-comments-post.php", scheme, host)
 
 	author := genAuthor()
@@ -144,7 +230,6 @@ func leaveReview(scheme, host, postID string, rating int) error {
 	values.Set("comment_parent", "0")
 
 	// NOTE: program can be parallelized if proxies are used
-	client := &http.Client{}
 	resp, err := client.PostForm(to, values)
 	if resp == nil {
 		return err
